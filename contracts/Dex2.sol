6bd0241d894fdc08ff930f30263f8c84dccdba7d
Jash2901 <jash.bohare@pedalsup.com>
2025-09-23 15:35:50 +0530

    Swap function compiled

---
+++ contracts/Dex2.sol
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {IPoolManager} from "@uniswap/v4-core/src/interfaces/IPoolManager.sol";
+import {PoolKey} from "@uniswap/v4-core/src/types/PoolKey.sol";
+import {SwapParams} from "@uniswap/v4-core/src/types/PoolOperation.sol";
+import {TickMath} from "@uniswap/v4-core/src/libraries/TickMath.sol";
+import {ActionConstants} from "@uniswap/v4-periphery/src/libraries/ActionConstants.sol";
+import {BalanceDelta, BalanceDeltaLibrary} from "@uniswap/v4-core/src/types/BalanceDelta.sol";
+
+contract Interactor{
+    IPoolManager public poolManager;
+
+    constructor(address _poolManager){
+        poolManager = IPoolManager(_poolManager);
+    }
+
+    function swapExactInputSingle (
+    PoolKey memory poolKey,
+    bool zeroForOne,
+    uint128 amountIn
+
+    ) external returns (uint128 amountOut) {
+        if (amountIn == ActionConstants.OPEN_DELTA) {
+            revert("OPEN_DELTA not supported");
+        }
+
+        int256 amountSpecified = -int256(uint256(amountIn));
+        uint160 sqrtPriceLimitX96 = zeroForOne ? TickMath.MIN_SQRT_PRICE + 1 : TickMath.MAX_SQRT_PRICE - 1;
+
+        SwapParams memory params = SwapParams({
+            zeroForOne: zeroForOne,
+            amountSpecified: amountSpecified,
+            sqrtPriceLimitX96: sqrtPriceLimitX96
+        });
+
+        BalanceDelta delta = poolManager.swap(poolKey, params, bytes(""));
+        amountOut = uint128(zeroForOne ? -BalanceDeltaLibrary.amount1(delta) : -BalanceDeltaLibrary.amount0(delta));
+
+    }
+
+    function _swap(
+        PoolKey memory poolKey,
+        bool zeroForOne,
+        uint128 amountIn,
+        uint128 amountOutMinimum
+    ) public returns (uint amountOut) {
+        BalanceDelta delta = poolManager.swap(
+            poolKey,
+            SwapParams({
+                zeroForOne: zeroForOne,
+                amountSpecified: -int256(uint256(amountIn)), 
+                sqrtPriceLimitX96: zeroForOne 
+                    ? TickMath.MIN_SQRT_PRICE + 1 
+                    : TickMath.MAX_SQRT_PRICE - 1
+            }),
+            bytes("") 
+        );
+
+        amountOut = uint128(
+            zeroForOne ? -delta.amount1() : -delta.amount0()
+        );
+
+        require(amountOut >= amountOutMinimum, "Too little received");
+    }
+
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19"
+      },
+      {
+        version: "0.8.24"
+      }
+    ]
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
